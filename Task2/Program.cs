46c51c0463bf8d8f94e2c8f6a4632893ba5028b7
Дмитрий <Sotka.SDI@gmail.com>
2026-04-20 23:17:54 +0300

    Создание Task2, Объявление переменных разных типов

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите свой возраст: ");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ваш возраст : {0}", age);
+        
+        Console.WriteLine("Введите ваше имя: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Ваше имя : {0}", name);
+        
+        Console.WriteLine("Где вы работаете?");
+        string corpName = Console.ReadLine();
+        Console.WriteLine("Вы работаете в : {0}", corpName);
+
+        bool a;
+        
+        Console.WriteLine("Введите свой вес");
+        double weight = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ваш вес:  {0}", weight);
+    }
+}
