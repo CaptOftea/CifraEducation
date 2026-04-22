93051e4946d25c5f020e686c53ca1cfbac6c8316
Дмитрий <Sotka.SDI@gmail.com>
2026-04-22 21:58:01 +0300

    Добавлено задание 2 и 3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,16 +1,21 @@
 ﻿namespace Task2;
 
+/// <summary>
+/// Этот класс посвящен Практике №1
+/// </summary>
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        // Задание 1.
+        
         Console.WriteLine("Введите свой возраст: ");
         int age = int.Parse(Console.ReadLine());
         Console.WriteLine("Ваш возраст : {0}", age);
         
-        Console.WriteLine("Введите ваше имя: ");
+        Console.WriteLine("Введите ваше имя: "); // совпадает с 2b и 3, поэтому объединил
         string name = Console.ReadLine();
-        Console.WriteLine("Ваше имя : {0}", name);
+        Console.WriteLine("Привет, : {0} !", name);
         
         Console.WriteLine("Где вы работаете?");
         string corpName = Console.ReadLine();
@@ -21,5 +26,54 @@ class Program
         Console.WriteLine("Введите свой вес");
         double weight = double.Parse(Console.ReadLine());
         Console.WriteLine("Ваш вес:  {0}", weight);
+
+        
+        // Задание 2.
+        
+        Console.WriteLine("Введите свой год рождения: ");
+        int bornYear = int.Parse(Console.ReadLine());
+        int myAge = DateTime.Now.Year - bornYear;
+        Console.WriteLine("Вам сейчас: {0}", myAge);
+
+        Console.WriteLine("Какая температура за окном?");
+        double temp = double.Parse(Console.ReadLine());
+        if (temp >= 0) Console.WriteLine("Ура лето");
+        else Console.WriteLine("О нет опять зима");
+
+        Console.WriteLine("Вы мужчина?");
+        bool gender = bool.Parse(Console.ReadLine());
+        switch (gender)
+        {
+            case true:
+                Console.WriteLine("Мужчина");
+                break;
+            case false:
+                Console.WriteLine("Женщина");
+                break;
+        }
+        
+        
+        // Задание 4.
+        
+        string quote, quote2;
+        string upper1, upper2;
+        do
+        {
+            Console.WriteLine("Введите любимую цитату");
+            quote = Console.ReadLine();
+            Console.WriteLine("Введите еще одну любимую цитату");
+            quote2 = Console.ReadLine();
+            upper1 = quote.ToUpper(); upper2 = quote2.ToUpper();
+
+            if (upper1 == upper2)
+            {
+                Console.WriteLine("Я сказал еще одну, а не ту же самую!!!");
+            }
+        }
+        while (upper1 == upper2);
+        
+        Console.WriteLine ("Ваша первая любимая цитата: '{0}' -прекрасна!, но вот вторая '{1}'-немного противоречива", quote, quote2);
+
+        
     }
 }
